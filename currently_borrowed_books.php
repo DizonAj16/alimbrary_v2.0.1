690a7baa328872441c6183a32b617b9ee2851886
arjec111 <arjecdizon99>
2024-03-22 14:23:18 +0800

    -m

--- currently_borrowed_books.php
+++ currently_borrowed_books.php
@@ -2,14 +2,15 @@
 // Include database connection
 include 'config.php';
 
-// Query to get all currently borrowed books
+// Query to get all currently borrowed books, ordered by borrow_date ascending
 $query_borrowed = "
     SELECT b.title, u.username, bb.borrow_date
     FROM borrowed_books bb
     JOIN books b ON bb.book_id = b.book_id
     JOIN users u ON bb.user_id = u.id
     LEFT JOIN return_history rh ON bb.borrow_id = rh.borrow_id
-    WHERE rh.borrow_id IS NULL"; // Fetch only records where the borrow ID does not exist in return_history, indicating the book has not been returned yet
+    WHERE rh.borrow_id IS NULL
+    ORDER BY bb.borrow_date ASC"; // Add ORDER BY clause to order by borrow_date ascending
 
 // Execute the query to get all currently borrowed books
 $result_borrowed = mysqli_query($conn, $query_borrowed);
